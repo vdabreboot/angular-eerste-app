4e33f9262728e94550f3a9c467f79f1f39e67aa8
Peter Kuda <PETER.KUDA@vdabcampus.be>
2023-09-04 13:01:43 +0200

    eerste update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Land } from './model/land'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  /*#templateUrl: './app.component.html',*/
+  template: `
+    <h1> {{title}}</h1>
+    <h2>Details van {{land.name}}</h2>
+    <div><label>id: <label>{{land.id}}<div>
+    <div><label>naam: </label>{{land.name}}<div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eerste-app';
+  title = 'Een lijstje van landen';
+  land: Land = 
+  {
+    id: 1,
+    name: 'Belgie'
+  }
 }
---
+++ src/app/model/land.ts
@@ -0,0 +1,4 @@
+export class Land {
+    id: number = -1;
+    name: string = '';
+}
